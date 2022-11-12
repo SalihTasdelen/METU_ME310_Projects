905da23ed0b7e5588ed5875f24b22bad70a5c2df
SalihTasdelen <salih.tasdelen@hotmail.com>
2022-11-12 15:56:42 +0300

    bisection method

--- e244690.py
+++ e244690.py
@@ -1,6 +1,39 @@
 #!/usr/bin/env python
 from collections import namedtuple
+from f import function
 
+'''
+Root Finding Methods
+'''
+
+def bisection_method(func, x_lower, x_upper, e_tolerance, max_iter, log_iter = False):
+    f_lower = func(x_lower)
+    x_old, xr, err = 0.0, 0.0, None
+    for i in range(1,max_iter):
+        xr = (x_lower + x_upper) / 2.0
+        f_xr = func(xr)
+        if xr != 0:
+            err = abs((xr - x_old) / xr) * 100.0
+        sign_test = f_lower * f_xr
+        if sign_test < 0: x_upper = xr
+        elif sign_test > 0:
+            x_lower = xr
+            f_lower = f_xr
+        else: err = 0
+
+        if log_iter: log_iteration('Bisection', i, xr, f_xr, err)
+        x_old = xr
+        if err and err < e_tolerance: return xr
+
+
+    log_warning(f'Maximum number of iterations have reached : MAX_ITER = {max_iter}')
+    return xr
+
+        
+
+'''
+Input and Error Handling
+'''
 Parameters = namedtuple('Parameters',
                         ['x_lower',
                          'x_upper',
@@ -13,13 +46,11 @@ def read_input(file_name = 'input.txt'):
         with open(file_name, 'r') as file:
             lines = file.readlines()
             if len(lines) < 4:
-                log_error('Invalid input format.')
-                exit_with_code(1)
+                log_error(1, 'Invalid input format')
             for i in range(4):
                 try: float(lines[i])
                 except:
-                    log_error(f'Invalid formatting for parameter {Parameters._fields[i]}.')
-                    exit_with_code(1)
+                    log_error(1, f'Invalid formatting for parameter {Parameters._fields[i]}')
 
             return Parameters(
                 x_lower = float(lines[0]),
@@ -29,17 +60,27 @@ def read_input(file_name = 'input.txt'):
             )
     
     except IOError:
-        log_error(f'No such file or directory : \'{file_name}\'')
-        exit_with_code(1)
+        log_error(1, f'No such file or directory : \'{file_name}\'')
 
-def log_error(msg = 'Unknown Error.'):
-    print(f'[Error] {msg}')
+'''
+Logging functions
+'''
 
-def exit_with_code(code = 0):
+def log_iteration(method,iter, xr, f_xr, err):
+    if err:
+        print(f'[{method}] {iter:3d} {xr:+3.8f} {f_xr:+2.8f} {err:3.8f}')
+    else:
+        print(f'[{method}] {iter:3d} {xr:+3.8f} {f_xr:+2.8f} ----')
+
+def log_warning(msg):
+    print(f'[Warning] {msg}.')
+
+def log_error(code : int, msg = 'Unknown Error'):
+    print(f'[Error] {msg}.')
     print(f'Exiting...\nExit Code {code}')
-    exit()
+    exit() 
 
 if __name__ == '__main__':
     param = read_input('sample_input.txt')
-    print(param[0])
+    bisection_method(function, param.x_lower, param.x_upper, param.e_tolerance, param.max_iter, True)
 
